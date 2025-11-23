3f163ebccc583144571273b56232d5ea52b1415d
Ulisses Damo <114618744+ulisses-damo@users.noreply.github.com>
2025-11-22 21:26:53 -0300

    Refatoração da config da Ia concluida

--- public/config.js
+++ public/config.js
@@ -1,6 +1,6 @@
-export const GEMINI_API_KEY = 'AIzaSyD9b0gXyoysVbDfDhEPCCFKEELFe3uDYyc';
+export const GEMINI_API_KEY = 'AIzaSyDLZ1MwGVvvIojrfCB5tu4U7qUgHbnABqk';
 
 export const GEMINI_CONFIG = {
-    model: 'gemini-1.5-flash',
-    apiUrl: 'https://generativelanguage.googleapis.com/v1/models/gemini-1.5-flash:generateContent'
+    model: 'gemini-2.5-flash',
+    apiUrl: 'https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent'
 };
